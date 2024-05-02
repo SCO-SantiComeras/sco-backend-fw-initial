3a64315da10594099af240139efdb407a85cdc1d
scomeras94 <scomeras@gmail.com>
2024-05-02 09:50:44 +0200

    v1

--- src/configuration/configuration-cors.ts
+++ src/configuration/configuration-cors.ts
@@ -1,5 +1,6 @@
 import { registerAs } from "@nestjs/config";
 
 export const configurationCors = registerAs("cors", () => ({
+  enabled: process.env.CORS_ENABLED == "true",
   origin: process.env.CORS_ORIGIN,
 }));
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import { LoggerService } from 'sco-backend-fw';
+import { AppModule } from './app.module';
+import { WebsocketsAdapter } from './modules/websockets/adapter/websockets-adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, 
@@ -8,7 +10,32 @@ async function bootstrap() {
       logger: new LoggerService(),
     }
   );
-  await app.listen(3000);
-  console.log(`[App] App started in 'http://localhost:3000'`);
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  if (configService.get('cors.enabled')) { 
+    app.enableCors({
+      origin: !configService.get("cors.origin")
+        ? []
+        : configService.get("cors.origin").split(','),
+      credentials: true,
+    });
+  }
+
+  if (configService.get('ws.enabled')) {
+    app.useWebSocketAdapter(
+      new WebsocketsAdapter(app, {
+        enabled: configService.get('ws.enabled'),
+        port: configService.get("ws.port"),
+        origin: !configService.get("ws.origin")
+          ? []
+          : configService.get("ws.origin").split(','),
+      })
+    );
+  }
+
+  await app.listen(configService.get('app.port'));
+  console.log(`[App] App started in 'http://${configService.get('app.host')}:${configService.get('app.port')}'`);
+  console.log(`[App] Environment loaded is: ${configService.get('app.env')}`);
 }
 bootstrap();
