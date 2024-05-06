041f549d904563ac9ea38395860ed4ca04c5827b
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-05-06 15:36:26 +0200

    ADD -createModel function

--- src/core/mongo-db/interfaces/imongo-basic.ts
+++ src/core/mongo-db/interfaces/imongo-basic.ts
@@ -1,6 +1,7 @@
-import { Model } from "mongoose";
+import { Model, Schema } from "mongoose";
 
 export interface IMongoBasic {
+    createModel(model: string, schema: Schema<any>, table: string): Model<any>;
     getModel(): Model<any>;
     findById(_id: string): Promise<any>;
 }
